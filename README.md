2c1edf54610dd0fc1196836d59f8d19948147a12
Ubuntu <ubuntu@highcore.novalocal>
2020-08-14 03:01:04 +0000

    No error on includes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # VSC_Test
 To test error in camke configuration received by VSC and share the same with MS development team
+
+Test Steps:
+1. No error on includes when no config is added, prior to creation of .vscode folder. -- First Commit
---
+++ helloWorld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <map>
+
+class Test {
+    public:
+        void printing();
+};
+
+void Test::printing() {
+    std::cout << "Hello World" << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    Test* t1 = new Test();
+    t1->printing(); 
+}
